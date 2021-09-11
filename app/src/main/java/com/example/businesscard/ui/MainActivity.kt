b7da94fdcdf30eab50a32d800ea0ae706e47a910
leotofoli <leoadt@gmail.com>
2021-09-11 18:59:13 -0300

    Preparação do ambiente e criação do App

---
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.businesscard.ui
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.businesscard.R
+import com.example.businesscard.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
