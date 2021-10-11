5faa6f1f86b1d234dd593391f9f9341585d6a7d4
leotofoli <leoadt@gmail.com>
2021-10-11 19:25:29 -0300

    Configuração do Room Database, parte 1

--- app/src/main/java/com/example/businesscard/App.kt
+++ app/src/main/java/com/example/businesscard/App.kt
@@ -1,6 +1,10 @@
 package com.example.businesscard
 
 import android.app.Application
+import com.example.businesscard.data.AppDataBase
+import com.example.businesscard.data.BusinessCardRepository
 
 class App: Application() {
+    val database by lazy { AppDataBase.getDataBase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
